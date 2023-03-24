f4085761f912063402cc0dca4daac4f52ae8fd55
mavis <mavis1020039@gmail.com>
2023-03-24 19:06:32 +0800

    manual saved

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,18 @@
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = 'Hello!';
+
+
+document.querySelector('p').onclick = function() {
+  alert('Ouch! Stop poking me!');
+}
+
+var myImg = document.querySelector('img');
+myImg.onclick = function() {
+  let mySrc = myImg.getAttribute('src');
+  if( mySrc === 'images/firefox-icon.png'){
+    myImg.setAttribute('src','images/firefox2.png');
+  } else {
+    myImg.setAttribute('src','images/firefox-icon.png');
+  }
+
+}
